36533e3c949818d8e0a76880c4b3e6569509326c
Nathan Timmerman <phillip.pluto@gmail.com>
2017-12-04 00:54:46 -0600

    pieder2, data to larger to parse

--- pieder2.py
+++ pieder2.py
@@ -6,14 +6,14 @@
 from __future__ import print_function
 import urllib2
 import xml.etree.ElementTree as ET
-import shlex, subprocess
+#import shlex, subprocess
 
 
-import sys#added so program can run in either python2 or python3
-import base64#added so program can run in either python2 or python3
+#import sys#added so program can run in either python2 or python3
+#import base64#added so program can run in either python2 or python3
 
 
-from itertools import tee, islice, chain, izip
+#from itertools import tee, islice, chain, izip
 
 def bingo():
     # Things to do when a match has been found
@@ -26,20 +26,12 @@ def main():
     file = urllib2.urlopen(url)
     data = file.read()
     file.close()
-    root=ET.fromstring(data)
-    for state in root.findall('State'):#as far as I know from above example there should only be one <State></State>
-        print ('state=',state)
-        for string in state.findall('String'):
-            # from above example there are multiple <String></String> so state.findall('String') is required
-            key=string.find('Key').text
-            value=string.find('Value').text
-            print ('key=',key)
-            print ('value=',value)
-            if (key== 'state'):
-                if (value=='idle'):
-                    print ('found match')
-                    bingo()#function to do something
-
+    tree = ET.parse(data)
+    root = tree.getgroot()
+    for item in root.findall('result-info'):#as far as I know from above example there should only be one <State></State>
+        print ('found match')
+        bingo()#function to do something
+#        print (item + '\n\n')
 
 if __name__ == '__main__':
     main()
